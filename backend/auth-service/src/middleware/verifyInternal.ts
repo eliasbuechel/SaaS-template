7cea459fa4adcd7e2d20b3288191bc964346db78
eliasbuchel <eli.buechel@hotmail.com>
2025-03-13 14:05:45 +0100

    updating infra docker compose to run application with updated auth service

---
+++ backend/auth-service/src/middleware/verifyInternal.ts
@@ -0,0 +1,5 @@
+import {Request, Response, NextFunction} from "express";
+
+export const verifyInternal = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    
+}
--- backend/shopify-service/src/lib/config/env.ts
+++ backend/shopify-service/src/lib/config/env.ts
@@ -1,12 +1,16 @@
 import BASE_ENV from "./baseEnv.js";
+import {getRequiredEnv} from "./envUtils.js";
+import TRANSFORMERS from "./transformers.js";
 
 interface CustomEnv {
     DATABASE_URL: string;
+    INTERNAL_AUTH_COMMUNICATION_SECRET: string;
 }
 
 
 const CUSTOM_ENV: CustomEnv = {
     DATABASE_URL: "", //getRequiredEnv('DATABASE_URL', TRANSFORMERS.STRING),
+    INTERNAL_AUTH_COMMUNICATION_SECRET: getRequiredEnv("INTERNAL_AUTH_COMMUNICATION_SECRET", TRANSFORMERS.STRING)
 };
 
 const ENV = {
